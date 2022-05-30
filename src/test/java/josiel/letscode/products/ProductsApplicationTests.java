e4620636eab5e333766d2d27c2ca8352d9bdbc99
jmmoura <josielmartinsmoura@gmail.com>
2022-05-30 01:39:37 -0300

    first commit

---
+++ src/test/java/josiel/letscode/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package josiel.letscode.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
